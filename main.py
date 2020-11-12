5d2871dd67b152c006eaa43023082609b2a5217a
Carpadiem <bezdudniy03@gmail.com>
2020-11-12 23:30:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import speech_recognition as sr
+import speech_recognition as sr
 import sys
 import webbrowser
 import pygame
@@ -8,59 +8,59 @@ file = 'speech.mp3'
 
 
 def play():
-    open('speech.mp3')
-    pygame.mixer.init()
-    pygame.mixer.music.load(file)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.mixer.music.get_pos() / 1000
+	open('speech.mp3')
+	pygame.mixer.init()
+	pygame.mixer.music.load(file)
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy():
+		pygame.mixer.music.get_pos() / 1000
 
 
 def tts(words):
-    pgtts = gTTS(text=words, lang='ru')
-    pgtts.save(file)
-    play()
+	pgtts = gTTS(text=words, lang='ru')
+	pgtts.save(file)
+	play()
 
 
 def talk(words):
-    print(words)
-    tts(words)
+	print(words)
+	tts(words)
 
 
 talk("Привет, чем я могу помочь вам?")
 
 
 def command():
-    return 0
+	return 0
 
 
 def command():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Говорите")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-    try:
-        zadanie = r.recognize_google(audio, language="ru-RU").lower()
-        print("Вы сказали: " + zadanie)
-    except sr.UnknownValueError:
-        talk("Я вас не поняла")
-        zadanie = command()
-    return zadanie
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("Говорите")
+		r.pause_threshold = 1
+		r.adjust_for_ambient_noise(source, duration=1)
+		audio = r.listen(source)
+	try:
+		zadanie = r.recognize_google(audio, language="ru-RU").lower()
+		print("Вы сказали: " + zadanie)
+	except sr.UnknownValueError:
+		talk("Я вас не поняла")
+		zadanie = command()
+	return zadanie
 
 
 def makesomething(zadanie):
-    if 'открыть сайт' in zadanie:
-        talk("Уже открываю")
-        url = 'https://itproger.com'
-        webbrowser.open(url)
-    elif 'стоп' in zadanie:
-        talk("Да, конечно, без проблем")
-        sys.exit()
-    elif 'имя' in zadanie:
-        talk("Меня зовут Сири")
+	if 'открыть сайт' in zadanie:
+		talk("Уже открываю")
+		url = 'https://itproger.com'
+		webbrowser.open(url)
+	elif 'стоп' in zadanie:
+		talk("Да, конечно, без проблем")
+		sys.exit()
+	elif 'имя' in zadanie:
+		talk("Меня зовут Сири")
 
 
 while True:
-    makesomething(command())
+	makesomething(command())
