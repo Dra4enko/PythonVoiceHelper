65d00f8b032ff25a2bebf58980ab12598808b25b
Carpadiem <bezdudniy03@gmail.com>
2020-11-17 16:23:48 +0300

    global update

---
+++ libcommands.py
@@ -0,0 +1,11 @@
+commands = {
+    # websites
+    'открой сайт вконтакте': 'http://vk.com',
+    'открой сайт питон': 'http://python.org',
+    'открой сайт гитхаб': 'http://github.com',
+
+    # programs
+    'открой файл стим': r"C:\Program Files (x86)\Steam\steam.exe",
+    'открой файл блокнот': r'C:\Windows\System32\notepad.exe',
+    'открой файл калькулятор': r'C:\Windows\system32\win32calc.exe'
+}
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import speech_recognition as sr
 import os
-import pyautogui as pag
 import webbrowser
 import pyttsx3
-
+import libcommands as libcmd
 
 def talk(words):
     engine = pyttsx3.init()
@@ -13,11 +12,11 @@ def talk(words):
     engine.runAndWait()
 
 
-def command():
+def listener():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("Говорите")
+        talk("Говорите")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
@@ -26,68 +25,55 @@ def command():
         task = r.recognize_google(audio, language="ru-RU").lower()
     except sr.UnknownValueError:
         talk("Я вас не поняла")
-        task = command()
+        task = listener()
 
     return task
 
 
-def command_sitename():
-    r = sr.Recognizer()
+def command(task):
+    for key, value in libcmd.commands.items():
+        if task == key:
+            if 'сайт' in task:
+                webbrowser.open(value)
 
-    with sr.Microphone() as source:
-        print("Введите название сайта: ")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-
-    try:
-        task = r.recognize_google(audio, language="ru-RU").lower()
-    except sr.UnknownValueError:
-        talk("Я вас не поняла.")
-        task = command_sitename()
-
-    return task
-
-
-# def makeSomething(task):
-#     if task == 'открой сайт':
-#         talk('Открываю сайт')
-#         webbrowser.open("http://" + command_sitename())
-#
-#     elif task == 'открой файл' or task == 'открой программу':
-#         talk("Открываю файл")
-#         talk("Введите путь до файла: ")
-#         path = input()
-#         os.startfile(path)
-
-def makeSomething(task):
-    if task == 'открой вк':
-        webbrowser.open("https://vk.com")
-        # webbrowser.open("http://" + command_sitename())
-    elif task == 'открой стим':
-        os.startfile(r"C:\Program Files (x86)\Steam\steam.exe")
-
-    elif task == 'открой дискорд':
-        os.startfile(r"C:\Users\User\AppData\Local\Discord\Update.exe")
-
-
-def command_inputtext():
-    r = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        print("Текст: ")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-
-    try:
-        task = r.recognize_google(audio, language="en-EN").lower()
-    except sr.UnknownValueError:
-        talk("Я вас не поняла.")
-        task = command_inputtext()
-
-    return str(task)
+            elif 'файл' in task:
+                os.startfile(value)
 
 
 while True:
-    makeSomething(command())
+    command(listener())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
